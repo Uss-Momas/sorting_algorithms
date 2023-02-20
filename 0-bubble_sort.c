9df7e9f336110152a144b29d0651fa471c891e41
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-20 22:00:54 +0200

    Bubble sort implementation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,13 +16,13 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = i + 1; j < size; j++)
+			for (j = 0; j < size - 1; j++)
 			{
-				if (array[i] > array[j])
+				if (array[j] > array[j + 1])
 				{
-					aux = array[i];
-					array[i] = array[j];
-					array[j] = aux;
+					aux = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = aux;
 					print_array(array, size);
 				}
 			}
