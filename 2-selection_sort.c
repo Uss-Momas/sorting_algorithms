ea4cf1d9e22a8b5cea08a0a7fe75b4e6a15de206
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-21 15:03:12 +0200

    Swapped flag variable to verify if swapped occured

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,18 +9,23 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index, aux;
+	int swapped;
 
 	if (size >= 2)
 	{
 		for (i = 0; i < size; i++)
 		{
+			swapped = 0;
 			min_index = i;
 			for (j = i; j < size - 1; j++)
 			{
 				if (array[min_index] > array[j + 1])
+				{
+					swapped = 1;
 					min_index = j + 1;
+				}
 			}
-			if (i + 1 != size)
+			if ((i + 1 != size) && swapped == 1)
 			{
 				aux = array[min_index];
 				array[min_index] = array[i];
