8951e1e0a77be065ce344bd4859772b91b456664
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-21 14:51:35 +0200

    Selection Sort algorithm implementation

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+#include <stdlib.h>
+/**
+ * selection_sort - selection sort algorithm implementation
+ * @array: the array containing the elements to sort
+ * @size: the size of the array
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index, aux;
+
+	if (size >= 2)
+	{
+		for (i = 0; i < size; i++)
+		{
+			min_index = i;
+			for (j = i; j < size - 1; j++)
+			{
+				if (array[min_index] > array[j + 1])
+					min_index = j + 1;
+			}
+			if (i + 1 != size)
+			{
+				aux = array[min_index];
+				array[min_index] = array[i];
+				array[i] = aux;
+				print_array(array, size);
+			}
+		}
+	}
+}
